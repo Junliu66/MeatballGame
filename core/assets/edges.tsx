8963388b4918e5e117868428806eff743f90d218
Cherryllei <chunlei.ku@gmail.com>
2017-11-15 21:10:48 -0800

    build the level one basemap.

---
+++ core/assets/edges.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="edges" tilewidth="1600" tileheight="1600" tilecount="1" columns="1">
+ <image source="edges.png" width="1600" height="1600"/>
+</tileset>
