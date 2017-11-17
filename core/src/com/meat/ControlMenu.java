399f0b04a30e55704d69acad4d0d3983d35ad1f8
Andre Zhu <andrez@uci.edu>
2017-11-16 17:58:07 -0800

    fixed menu error

--- core/src/com/meat/ControlMenu.java
+++ core/src/com/meat/ControlMenu.java
@@ -2,13 +2,18 @@ package com.meat;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
@@ -34,11 +39,17 @@ public class ControlMenu extends Stage implements Screen {
     String leftText = "Left: ";
     String rightText = "Right: ";
 
-    ArrayList<TextField> textFields;
-    TextField upField;
-    TextField downField;
-    TextField leftField;
-    TextField rightField;
+    ArrayList<TextButton> textFields;
+    TextButton upField;
+    TextButton downField;
+    TextButton leftField;
+    TextButton rightField;
+    TextButton blankButton;
+
+    //flags for TextButtons
+    TextButton currentClicked;
+
+    float buttonWidth = 100f;
 
     TextButton playerUp;
     TextButton playerDown;
@@ -50,7 +61,7 @@ public class ControlMenu extends Stage implements Screen {
     public ControlMenu(MainGame game){
         this.game = game;
         menuText = new ArrayList<Label>();
-        textFields = new ArrayList<TextField>();
+        textFields = new ArrayList<TextButton>();
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false, viewportWidth, viewportHeight);
@@ -62,6 +73,8 @@ public class ControlMenu extends Stage implements Screen {
 
 
         Skin labelSkin = new Skin(Gdx.files.internal("uiskin.json"));
+        blankButton = new TextButton("", labelSkin);
+        currentClicked = blankButton;
 
         Label controlLabel = new Label(controlText, labelSkin);
 
@@ -69,34 +82,43 @@ public class ControlMenu extends Stage implements Screen {
         Label upLabel = new Label(upText, labelSkin);
         upLabel.setPosition(textX, 300f);
         menuText.add(upLabel);
-        upField = new TextField("", labelSkin);
+        upField = new TextButton(Input.Keys.toString(Config.player1Up), labelSkin);
+        //upField.addListener();
         textFields.add(upField);
 
         Label downLabel = new Label(downText, labelSkin);
         menuText.add(downLabel);
-        downField = new TextField("", labelSkin);
+        downField = new TextButton(Input.Keys.toString(Config.player1Down), labelSkin);
         textFields.add(downField);
 
         Label leftLabel = new Label(leftText, labelSkin);
         menuText.add(leftLabel);
-        leftField = new TextField("", labelSkin);
+        leftField = new TextButton(Input.Keys.toString(Config.player1Left), labelSkin);
         textFields.add(leftField);
 
         Label rightLabel = new Label(rightText, labelSkin);
         menuText.add(rightLabel);
-        rightField = new TextField("", labelSkin);
+        rightField = new TextButton(Input.Keys.toString(Config.player1Right), labelSkin);
         textFields.add(rightField);
+
+        upField.addListener(getInputListener());
 
         displayTable.add(controlLabel);
         displayTable.row();
+        Gdx.input.setInputProcessor(this);
+        addListener(getInputListener());
         for(int i = 0; i < menuText.size(); i++){
             //this.addActor(label);
             Label label = menuText.get(i);
-            TextField textField = textFields.get(i);
+            TextButton textField = textFields.get(i);
+            textField.getStyle().up.setMinWidth(buttonWidth);
+            textField.addListener(getChangeControlListener());
+            //textField.addListener(getInputListener());
             displayTable.add(label);
             displayTable.add(textField);
             displayTable.row();
         }
+
 
         this.addActor(displayTable);
         displayTable.setFillParent(true);
@@ -138,4 +160,50 @@ public class ControlMenu extends Stage implements Screen {
     public void dispose(){
 
     }
+
+    public ClickListener getChangeControlListener(){
+        return new ClickListener(){
+            public void clicked(InputEvent event, float x, float y){
+                Actor clicked = event.getListenerActor();
+
+                currentClicked = (TextButton) clicked;
+                currentClicked.setText("");
+            }
+        };
+    }
+
+    public InputListener getInputListener(){
+        return new InputListener(){
+            @Override
+            public boolean keyDown(InputEvent event, int keycode){
+                return true;
+            }
+
+            public boolean keyTyped(InputEvent event, char character){
+                return true;
+            }
+            @Override
+            public boolean keyUp(InputEvent event, int keycode){
+                String keycodeString = Input.Keys.toString(keycode);
+                if(currentClicked == upField){
+                    Config.player1Up = keycode;
+                }
+                else if(currentClicked == downField){
+                    Config.player1Down = keycode;
+                }
+                else if(currentClicked == leftField){
+                    Config.player1Left = keycode;
+                }
+                else if(currentClicked == rightField){
+                    Config.player1Right = keycode;
+                }
+                currentClicked.setText(keycodeString);
+                currentClicked = blankButton;
+
+                return true;
+            };
+        };
+    }
+
+
 }
