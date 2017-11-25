883747d365378c537c2d3d905c65275912837445
Stefano Mauri <stefanom@gmail.com>
2017-11-25 00:41:13 -0800

    fixed MainMenu buttons, added save game functionality

--- core/src/com/meat/MainGame.java
+++ core/src/com/meat/MainGame.java
@@ -4,10 +4,21 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class MainGame extends Game
 {
     public SpriteBatch batch;
     public BitmapFont font;
+    int totalScore, numTrophies;
+    int lvlTrophies[] = {0, 0, 0, 0, 0, 0};
+
     public MeatGame meatGame = null;
 
     public void create() {
@@ -25,5 +36,41 @@ public class MainGame extends Game
     public void dispose() {
         batch.dispose();
         font.dispose();
+    }
+
+    public int getTrophies(){
+        return numTrophies;
+    }
+
+    public int[] getLvlTrophies() {
+        return lvlTrophies;
+    }
+
+    public void saveGame()
+            throws IOException {
+        String str = totalScore + "\n" + numTrophies + "\n";
+        for (int i = 0; i < 6; i++)
+        {
+            str += lvlTrophies[i] + "\n";
+        }
+
+        Path path = Paths.get("save.txt");
+        byte[] strToBytes = str.getBytes();
+        Files.deleteIfExists(path);
+        Files.write(path, strToBytes);
+
+        String read = Files.readAllLines(path).get(0);
+    }
+
+    public void loadGame ()
+            throws FileNotFoundException {
+        File file = new File("save.txt");
+        Scanner sc = new Scanner(file);
+        totalScore = Integer.parseInt(sc.nextLine());
+        numTrophies = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < 6; i++)
+        {
+            lvlTrophies[i] = Integer.parseInt(sc.nextLine());
+        }
     }
 }
