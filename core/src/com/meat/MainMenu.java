d034a68c7e8c7db641412ba54cc2cc8b0b2979f8
zhangJunliu <lnstdcyw@gmail.com>
2017-12-07 16:23:00 -0800

    update map

--- core/src/com/meat/MainMenu.java
+++ core/src/com/meat/MainMenu.java
@@ -52,7 +52,7 @@ public class MainMenu implements Screen {
     int buttonYHelp = 30;
     int buttonYSettings = 30;
     int buttonYExit = 30;
-    int buttonYPlay = 90;
+    int buttonYPlay = 100;
     int buttonXSave = 260;
     int buttonXLoad = 560;
     int buttonYSave = 100;
